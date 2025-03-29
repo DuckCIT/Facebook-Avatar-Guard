f7665d4232856c42757763fe9eadf3d09d9d207f
DuckCIT <nguyentronhduc@gmail.com>
2025-03-29 23:01:56 +0700

    Add version check and update notification functionality

--- background.js
+++ background.js
@@ -72,3 +72,35 @@ function toggleAvatarGuard(userId, fbDtsg, toggle) {
     .then(response => response.json())
     .then(res => res.errors ? Promise.reject(res) : res);
 }
+
+function checkUpdate() {
+    fetch("https://raw.githubusercontent.com/DuckCIT/Facebook-Avatar-Guard/main/data/version.json")
+        .then(response => response.json())
+        .then(data => {
+            const currentVersion = chrome.runtime.getManifest().version;
+
+            if (data.version > currentVersion) {
+                chrome.storage.local.get(["update_notified"], (result) => {
+                    if (!result.update_notified) {
+                        chrome.storage.local.set({ update_notified: true });
+                        chrome.notifications.create("update_notification", {
+                            type: "basic",
+                            iconUrl: "img/icon.png",
+                            title: "New Update Available!",
+                            message: `A new version (${data.version}) is available. Click here to update.`,
+                            priority: 2
+                        });
+                        chrome.notifications.onClicked.addListener((notificationId) => {
+                            if (notificationId === "update_notification") {
+                                chrome.tabs.create({ url: "https://github.com/DuckCIT/Facebook-Avatar-Guard" });
+                            }
+                        });
+                    }
+                });
+            }
+        })
+        .catch(() => {});
+}
+
+chrome.runtime.onStartup.addListener(checkUpdate);
+chrome.runtime.onInstalled.addListener(checkUpdate);
